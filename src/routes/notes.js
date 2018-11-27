80b988033d05ed06c750b76fcbb4739e5dabedf8
Renzo Rios <rios.renzo@pucp.pe>
2018-11-27 13:15:07 -0500

    Add and Show Activities

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,3 +1,39 @@
 const router = require('express').Router();
 
+const Note = require('../models/Note')
+
+router.get('/notes/add', (req, res) => {
+  res.render('notes/new-note')
+});
+
+router.get('/notes', async (req, res) => {
+ const notes = await Note.find().sort({date:'desc'});
+ res.render('notes/all-notes', {notes})
+});
+
+router.post('/notes/new-note', async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+ 
+  if(!title){
+    errors.push({text :'Ingrese el titulo'})
+  }
+  if(!description){
+    errors.push({text: 'Ingrese una descripción'})
+  }
+  if(errors.length > 0){
+    res.render('notes/new-note', {
+      errors,
+      title,
+      description
+    });
+  }
+  else{
+   const newNote = new Note({title, description});
+    console.log(newNote);  
+    await newNote.save();
+    res.redirect('/notes')
+  }
+});
+
 module.exports = router;
