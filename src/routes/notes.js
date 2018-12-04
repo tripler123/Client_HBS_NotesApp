a2118430caf9f1fc1d8d480fd2d2bc3fbe1d62bc
Renzo Rios <rios.renzo@pucp.pe>
2018-12-04 13:26:13 -0500

    Finalizacion de CRUD

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -11,6 +11,26 @@ router.get('/notes', async (req, res) => {
  res.render('notes/all-notes', {notes})
 });
 
+
+router.get('/notes/edit/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  res.render('notes/edit-note', {note})
+})
+
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+  const { title, description} = req.body;
+  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  req.flash('success_msg', 'Nota Actualizada')
+  res.redirect('/notes')
+})
+
+router.delete('/notes/delete/:id' , async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  req.flash('success_msg', 'Nota Eliminada')
+  res.redirect('/notes');
+})
+
 router.post('/notes/new-note', async (req, res) => {
   const { title, description } = req.body;
   const errors = [];
@@ -32,6 +52,7 @@ router.post('/notes/new-note', async (req, res) => {
    const newNote = new Note({title, description});
     console.log(newNote);  
     await newNote.save();
+    req.flash('success_msg', 'Nota Agregada')
     res.redirect('/notes')
   }
 });
